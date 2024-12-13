1fa4124005217bcae0f49436754a3ef4d7e8820e
12th-ai <mihigofololive@gmail.com>
2024-12-13 14:28:38 +0200

    add google tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense,useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import router from './Routers/Index';
 import { Helmet } from 'react-helmet'; // Import Helmet for SEO
